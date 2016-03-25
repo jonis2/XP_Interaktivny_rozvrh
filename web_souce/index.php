6f95607d3f1b058b8f796df64f061a9cfe6622a1
jonis2 <jonismajo@gmail.com>
2016-03-25 11:20:08 +0100

    zakladyn template webu

---
+++ web_souce/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Interaktívny Rozvrh</title>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/bootstrap-theme.css" rel="stylesheet">
+  </head>
+    <body>
+    <div class="container">
+       <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Rozvrh</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Zaregistrovať sa </a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Prihlásiť sa</a></li>
+    </ul>
+  </div>
+</nav>
+    <section>
+      <p>ROZVRH VYHLADANIE</p><!-- TODO VYHLADAVANIE A ROZVRH -->
+    </section>
+<div class="container">
+        <footer class="footer" >
+          <hr>
+          <p class="lead" > Marián Jonis a Ľubomír Hrzič </p>
+          <div  class="lead">Email : <a href="mailto:jonismajo@gmail.com" > jonismajo@gmail.com </a> , <a href="mailto:hrzic@azet.sk" > hrzic@azet.sk </a></div>
+        </footer>
+      </div>
+      </div>  
+    </body>
+</html>                                                     
